45a3506076c68ca2cdab0b0376b4938b9119c348
fisker Cheung <lionkay@gmail.com>
2021-06-17 19:07:50 +0800

    tests/format/css/yaml/dirty.css

--- tests/format/css/yaml/jsfmt.spec.js
+++ tests/format/css/yaml/jsfmt.spec.js
@@ -1,1 +1,27 @@
-run_spec(__dirname, ["css"]);
+const { outdent } = require("outdent");
+
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: [
+      {
+        code: outdent`
+          ---
+          hello:     world
+          a:${" ".repeat(7)}
+                      - 123
+                      - 666
+          ---
+
+          .class {
+
+
+
+          }
+        `,
+        name: "dirty.css",
+      },
+    ],
+  },
+  ["css"]
+);
