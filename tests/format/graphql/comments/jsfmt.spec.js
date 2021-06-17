68fd4d9d4bfe6f8e622a89c1ed053956e95a92bf
fisker Cheung <lionkay@gmail.com>
2021-06-17 19:16:32 +0800

    tests/format/graphql/comments/fields.graphql

--- tests/format/graphql/comments/jsfmt.spec.js
+++ tests/format/graphql/comments/jsfmt.spec.js
@@ -1,1 +1,18 @@
-run_spec(__dirname, ["graphql"]);
+const { outdent } = require("outdent");
+
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: [
+      {
+        code: outdent`
+          query {\u0020
+            someField # Trailing comment
+          }\n\n\n
+        `,
+        name: "fields.graphql",
+      },
+    ],
+  },
+  ["graphql"]
+);
