b1e0c983ca33519d60d4d6c04a8c81732a7ec906
fisker Cheung <lionkay@gmail.com>
2021-06-17 19:21:49 +0800

    tests/format/graphql/interface/separator-detection.graphql

--- tests/format/graphql/interface/jsfmt.spec.js
+++ tests/format/graphql/interface/jsfmt.spec.js
@@ -1,1 +1,43 @@
-run_spec(__dirname, ["graphql"]);
+const { outdent } = require("outdent");
+
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: [
+      {
+        code: outdent`
+          type Type1 implements A, B, C, D
+          # {&&&&&&&&
+          # Should separate by ',' not '&'
+          {a: a}
+
+          type Type2 implements A, B, C, D
+          # &&&&{}&&&&
+          # Should separate by ',' not '&'
+          {a: a}
+
+          type Type3 implements A,\u0020
+          # &&&&&&&& comment line 1
+            \u0020# &&&&&&&& comment line 2
+          B& C, D
+          {a: a}
+
+          type Type4 implements A
+          # &&&&&&&& comment line 1
+          ,\u0020
+            \u0020# &&&&&&&& comment line 2
+          B& C, D
+          {a: a}
+
+          type Type5 implements A\u0020
+          # &&&&&&&& comment line 1
+            \u0020# &&&&&&&& comment line 2
+          ,B& C, D
+          {a: a}\n
+        `,
+        name: "separator-detection.graphql",
+      },
+    ],
+  },
+  ["graphql"]
+);
