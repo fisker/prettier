0694e9e04eed76b3b95e0e1946879c44c7999932
fisker Cheung <lionkay@gmail.com>
2021-06-17 19:11:48 +0800

    tests/format/flow/generic/break.js

--- tests/format/flow/generic/jsfmt.spec.js
+++ tests/format/flow/generic/jsfmt.spec.js
@@ -1,2 +1,25 @@
-run_spec(__dirname, ["flow", "babel"]);
-run_spec(__dirname, ["flow", "babel"], { trailingComma: "all" });
+const { outdent } = require("outdent");
+
+const fixtures = {
+  dirname: __dirname,
+  snippets: [
+    {
+      code: outdent`
+        var X = {
+          perform: function<${" ".repeat(4)}
+            A, B, C, D, E, F, G,${" ".repeat(5)}
+            T: (a: A, b: B, c: C, d: D, e: E, f: F) => G // eslint-disable-line space-before-function-paren
+          >(${" ".repeat(5)}
+            method: T, scope: any,${" ".repeat(5)}
+            a: A, b: B, c: C, d: D, e: E, f: F,${" ".repeat(4)}
+          ): G {
+          }
+        }\n
+      `,
+      name: "break.js",
+    },
+  ],
+};
+
+run_spec(fixtures, ["flow", "babel"]);
+run_spec(fixtures, ["flow", "babel"], { trailingComma: "all" });
