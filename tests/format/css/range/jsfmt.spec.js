ec4e6c36c0b97e01a6a266c887c07035200f16f8
fisker Cheung <lionkay@gmail.com>
2021-06-17 19:04:30 +0800

    tests/format/css/range/issue2267.css

--- tests/format/css/range/jsfmt.spec.js
+++ tests/format/css/range/jsfmt.spec.js
@@ -1,1 +1,9 @@
-run_spec(__dirname, ["css"]);
+run_spec(
+  {
+    dirname: __dirname,
+    snippets: [
+      { code: ".x{}<<<PRETTIER_RANGE_END>>> \n", name: "issue2267.css" },
+    ],
+  },
+  ["css"]
+);
