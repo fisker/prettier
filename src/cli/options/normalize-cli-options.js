dc32752e69d99393e86506ff9a625adc1570cea5
fisker Cheung <lionkay@gmail.com>
2022-08-18 14:23:15 +0800

    Move `cliDescriptor`

--- src/cli/options/normalize-cli-options.js
+++ src/cli/options/normalize-cli-options.js
@@ -1,11 +1,27 @@
 import chalk from "chalk";
 import leven from "leven";
+import vnopts from "vnopts";
 import { normalizeCliOptions as prettierNormalizeCliOptions } from "../prettier-internal.js";
+
+
+const cliDescriptor = {
+  key: (key) => (key.length === 1 ? `-${key}` : `--${key}`),
+  value: (value) => vnopts.apiDescriptor.value(value),
+  pair: ({ key, value }) =>
+    value === false
+      ? `--no-${key}`
+      : value === true
+      ? cliDescriptor.key(key)
+      : value === ""
+      ? `${cliDescriptor.key(key)} without an argument`
+      : `${cliDescriptor.key(key)}=${value}`,
+};
 
 function normalizeCliOptions(options, optionInfos, opts) {
   return prettierNormalizeCliOptions(options, optionInfos, {
     colorsModule: chalk,
     levenshteinDistance: leven,
+    descriptor: cliDescriptor,
     ...opts,
   });
 }
