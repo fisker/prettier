1ffacc71ddaaeddaaab4c01ee0f214728a269551
fisker <lionkay@gmail.com>
2025-02-17 17:49:45 +0800

    Tests

--- tests/format/js/class-extends/format.test.js
+++ tests/format/js/class-extends/format.test.js
@@ -3,6 +3,7 @@ runFormatTest(import.meta, ["babel", "flow", "typescript"], {
     acorn: ["tuple-and-record.js"],
     espree: ["tuple-and-record.js"],
     meriyah: ["tuple-and-record.js"],
+    oxc: ["tuple-and-record.js"],
     typescript: ["tuple-and-record.js"],
     flow: ["tuple-and-record.js"],
   },
