285496a711e749d6f0439af521d6004295289efe
fisker <lionkay@gmail.com>
2026-01-14 02:15:22 +0800

    Refactor `handleCommentInEmptyParens`

--- src/language-js/comments/handle-comments.js
+++ src/language-js/comments/handle-comments.js
@@ -658,14 +658,8 @@ function handleCommentInEmptyParens({ comment, enclosingNode, options }) {
     return true;
   }
 
-  // This condition should be removed, but excluded for function parameters in #18615 to make PRs smaller
-  if (comment.placement !== "remaining") {
-    return false;
-  }
-
   // Only add dangling comments to fix the case when no params are present,
   // i.e. a function without any argument.
-
   const functionNode = isRealFunctionLikeNode(enclosingNode)
     ? enclosingNode
     : enclosingNode.type === "MethodDefinition" ||
