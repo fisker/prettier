4380c0a7a81d1b563dd6cf6adf4634a751591b71
fisker <lionkay@gmail.com>
2026-01-30 02:24:41 +0800

    flatBinaryishExpression

---
+++ src/language-js/utilities/flat-binaryish-expression.js
@@ -0,0 +1,18 @@
+import { shouldFlatten } from "./should-flatten.js";
+
+function* flatBinaryishExpression(node, selectors = []) {
+  for (const property of ["left", "right"]) {
+    const child = node[property];
+    const childSelectors = [...selectors, property];
+    if (
+      node.type === child.type &&
+      shouldFlatten(node.operator, child.operator)
+    ) {
+      yield* flatBinaryishExpression(child, childSelectors);
+    } else {
+      yield { node: child, selectors: childSelectors };
+    }
+  }
+}
+
+export { flatBinaryishExpression };
