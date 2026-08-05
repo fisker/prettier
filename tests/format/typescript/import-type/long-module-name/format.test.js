451d8b4023818d1b4864923ba44c908eed7cd215
fisker <lionkay@gmail.com>
2026-08-05 09:55:33 +0800

    Fix

--- tests/format/typescript/import-type/long-module-name/format.test.js
+++ tests/format/typescript/import-type/long-module-name/format.test.js
@@ -1,1 +1,11 @@
-runFormatTest(import.meta, ["typescript"]);
+runFormatTest(import.meta, ["typescript"], {
+  errors: {
+    "acorn-ts": [
+      "long-module-name.ts",
+      "long-module-name2.ts",
+      "long-module-name3.ts",
+      "long-module-name4.ts",
+      "long-module-name5.ts",
+    ],
+  },
+});
